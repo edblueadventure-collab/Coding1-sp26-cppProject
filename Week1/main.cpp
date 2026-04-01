0a878e6b5a4b04fda551dc32203a216f11b6f4eb
edblueadventure-collab <edblueadventure@gmail.com>
2026-04-01 17:45:42 -0500

    create main.cpp

---
+++ Week1/main.cpp
@@ -0,0 +1,14 @@
+//Ethan Dingus Coding 1 spring 2026
+// Using VS codium and its intergrated terminal
+// to learn about variables and logic
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    //'\n' or 'backslah n'makes a new line
+    cout << "All about variable and logic!\n";
+
+
+}

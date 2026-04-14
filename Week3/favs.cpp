06740ac8f88f98ad1740586fe74e1504143ecd95
edblueadventure-collab <edblueadventure@gmail.com>
2026-04-14 10:50:58 -0500

    Create favs.cpp

---
+++ Week3/favs.cpp
@@ -0,0 +1,30 @@
+// Ethan Dingus
+// lets create a list of favorites
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    while(true) {
+        cout << "What would you like to do?\n";
+        cout << "You cna 'add', 'eidt', 'remove', 'show', or 'quit'.\n";
+
+        string input;
+        getline(cin, input);
+        
+        }
+        if(input == "quit") {
+            cout << "Thanks for playing\n";
+            break;
+        }
+        else if(input == "add") {
+            ///
+        }
+        else {
+            cout << "I don't recongize that input.\n";
+        }
+    }

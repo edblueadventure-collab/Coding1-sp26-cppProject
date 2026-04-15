5992417b651e4f60ef3fd3577dca44078fdfc91f
edblueadventure-collab <edblueadventure@gmail.com>
2026-04-15 17:58:27 -0500

    Update favs.cpp

--- Week3/favs.cpp
+++ Week3/favs.cpp
@@ -11,20 +11,28 @@ using namespace std;
 int main() {
     while(true) {
         cout << "What would you like to do?\n";
-        cout << "You cna 'add', 'eidt', 'remove', 'show', or 'quit'.\n";
+        cout << "You have 5 options, 'add', 'edit', 'remove', 'show', 'quit'.\n";
 
         string input;
         getline(cin, input);
-        
-        }
+
         if(input == "quit") {
-            cout << "Thanks for playing\n";
+            cout << "Thanks for playing.\n";
             break;
         }
         else if(input == "add") {
-            ///
+            vector<string> playerGames;
+
+            while(playerGames.size() == 5) {
+                cout << "Add 5 of your favorite games to the list.\n";
+                string inupt;
+                cin >> input;
+
+                playerGames.push_back(input);
+            }
         }
         else {
-            cout << "I don't recongize that input.\n";
+            cout << "I don't recognize that input.\n";
         }
     }
+}

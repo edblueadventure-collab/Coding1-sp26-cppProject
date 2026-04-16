137e63918be6d38c741f689aa17fb573a000da5d
edblueadventure-collab <edblueadventure@gmail.com>
2026-04-16 16:40:29 -0500

    Update favs.cpp

--- Week3/favs.cpp
+++ Week3/favs.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 
 using namespace std;
+vector<string> playerGames;
 
 int main() {
     while(true) {
@@ -19,28 +20,56 @@ int main() {
         if(input == "quit") {
             cout << "Thanks for playing.\n";
             break;
-        }
+        }//end of quit
+
         else if(input == "add") {
-            vector<string> playerGames;
 
             while(playerGames.size() < 5) {
                 cout << "Add 5 of your favorite games to the list.\n";
-                string inupt;
-                cin >> input;
+                getline(cin, input);
 
                 playerGames.push_back(input);
 
                 
             }
+            
+        }//end of add
+        else if(input == "edit") {
+            string oldGame, newGame;
+
+            cout << "Which game do you want to edit: ";
+            cin >> oldGame;
+
+            auto iter = find(playerGames.begin(), playerGames.end(), oldGame);
+
+            if(iter != playerGames.end()) {
+                cout << "Provide a new game title: ";
+                cin >> newGame;
+                *iter = newGame;
+
+            } else {
+                cout << "The game is not found.\n";
+            }
+        }//end of edit
+
+        else if(input == "remove") {
+            
+        }
+
+
+        
+
+        else if(input == "show") {
             cout << "Here is the list you made!\n";
+
             for(vector<string>:: iterator iter = playerGames.begin(); iter != playerGames.end(); iter++) {
                 cout << *iter << endl;
             }
-        }
-       
+       }//end of show
+
         
         else {
             cout << "I don't recognize that input.\n";
         }
-    }
-}
+    }//end of while(true)
+}//end of main

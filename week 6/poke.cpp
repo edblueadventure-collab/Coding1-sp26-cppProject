6b1c7769e520b0dd43ad1d18099e832ae1b7987a
edblueadventure-collab <edblueadventure@gmail.com>
2026-05-06 23:11:46 -0500

    create psuedo for poke

---
+++ week 6/poke.cpp
@@ -0,0 +1,106 @@
+//Ethan Dingus coding 1
+//creating a pokemon inspired tomagatchi
+
+
+//create a class of variables titled poke
+//for class variable.. 
+// name
+// health
+// damage
+// hunger
+// is it alive
+
+
+
+//once i have the critter create a constructor
+
+
+//set it name 
+//set health, damage, hunger for starting values
+//mark the poke as alive
+
+//set name variable
+//get name variable
+
+//set health variable
+//get health variable
+
+//set damage variable 
+//get damage variable
+
+//set hunger variable
+//get hunger variable
+
+//set alive variable
+//get alive variable
+
+
+
+//Write out the different functions for the poke
+
+//create a 'hello' function
+    //show name, health, damage, and hunger
+    //if hunger goes above 5
+    //have the program mention it
+
+
+//create a 'feed' function
+    //if you feed, it lowers the number
+    //then mention that the critter ate
+
+
+//create a 'training' function
+    //if hunger is above 10...
+    //say that the poke is starving
+    //if hunger is above 5 warn the player
+    //add 1 to the hunger level
+    //flip a coin
+        //heads = increase health
+        //talis = increase damage
+    //tell the player what has improved
+
+
+//create a 'battle' function
+    //if hunger is above 10 stop...
+        //and say its hungry
+    //if hunger is above 5, warn the player but continue
+    //add 1 to the hunger
+    //keep going back and fourth in the battle
+    //until someone wins or the player runs
+        //ask the player
+            //attack
+            //heal
+            //or run 
+        //if attack - subtract damage from enemy health
+        //if heal - add some health back 
+        //if run - end battle
+        //if the enemy is alive, it fights back
+        //check to see if either side has fallen in battle
+        //end the battle if so
+
+
+
+//main 
+
+//start
+    //open the names file
+    //read the name in the list
+    //then close the file
+
+
+//name critter function
+    //ask the player for input if they wish to name the poke
+    //if random every, pick one in the list
+    //create poke with that name
+
+
+//create a while loop (repeat this until player is done)
+    //show the options to the player
+        //feed
+        //train
+        //listen
+        //battle
+        //quit
+    //do what the player asked
+    //if battle choose a poke at random from the names on the list
+

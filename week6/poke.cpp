6984daf91b0c38b40665e620e00ae4851e6f59ca
edblueadventure-collab <edblueadventure@gmail.com>
2026-05-12 02:25:11 -0500

    Updated Poke.cpp and made names.txt

--- week6/poke.cpp
+++ week6/poke.cpp
@@ -12,8 +12,6 @@ using namespace std;
 
 
 //create a class of variables titled poke
-
-vector<string> pokeNames = {"Charmander", "Squritle", "Bulbasaur", "Pikachu", "Oshawott"};
 //craete a class of variables
 class poke {
 private:
@@ -25,14 +23,26 @@ private:
     int damage;
     // hunger
     int hunger;
+    //defense
+    int defense;
     // is it alive
     bool alive;
+    
 
 public:
-    poke(string givenName, int givenHealth, int givenDamage) {
+    poke(string givenName, int givenHealth, int givenDamage, int givenDefense) {
         name = givenName;
         health = givenHealth;
         damage = givenDamage;
+        defense = givenDefense;
+        hunger = 0;
+        alive = true;
+    }
+    poke(vector<string>& names) {
+        name = names[rand() % names.size()];
+        health = rand() % 10 + 10;
+        damage = rand() % 5 + 3;
+        defense = rand() % 5 + 3;
         hunger = 0;
         alive = true;
     }
@@ -57,6 +67,13 @@ public:
     int getDamage() {
         return damage;
     }
+    
+    void setDefense(int givenDefense) {
+        defense = givenDefense;
+    }
+    int getDefense() {
+        return defense;
+    }
 
     void setHunger(int givenHunger) {
         hunger = givenHunger;
@@ -78,41 +95,139 @@ public:
     }
 
     void listen() {
+        //show name, health, damage, and hunger
         cout << "Name: " << name << "\n";
         cout << "Health: " << health << "\n";
         cout << "Damage: " << damage << "\n";
         cout << "Hunger: " << hunger << "\n";
-    }
     
+        if(hunger <= 5) {
+                cout << "I'm feeling good.\n";
+            }   
+            //if hunger goes above 5
+            else if(hunger <= 10) {
+                cout << "I'm a bit hungry.\n";
+            }
+            else {
+                cout << "FEED ME NOW!\n";
+            }       
+                
+    }
+    void train() {
+        if(hunger > 10) {
+            cout << "STOP your Poke needs food.\n";
+        }
+        else {
+            if(hunger > 5) {
+            cout << "Hey you can train,\nbut you need to feed your Poke.";
+            }
+            hunger += 1;
+            if(rand() % 2 == 0) {
+                health += 1;
+                cout << "Increase to Health";
+            }
+            else {
+                damage += 1;
+                cout << "Increase to Damage";
+            }
+        }    
+    }
+    void battle(vector<string>& names) {
+        string input;
+        if(hunger > 10) {
+            cout << "STOP! You can't battle with your Poke in that condition.\n";
+        }
+        else {
+            if(hunger > 5) {
+                    cout << "You can fight, but feed your poke after.\n";
+            }
+            hunger += 1;
+            poke enemy(names);
+            cout << "A wild " << enemy.getName() << " approaches!\n";
+            cout << "Enemy health: " << enemy.getHealth() << "\n";
+            cout << "Enemy damage: " << enemy.getDamage() << "\n";
+            if(rand() % 2 == 0) {
+                cout << "You go first!";
+            }    
+            else {
+                cout << "The wild pokemon goes first!";
+            }
+                
+        
+            //create a while loop to make battle
+            while(enemy.getHealth() > 0 && health > 0) {
+                cout << "What do you do?\n";
+                cout << "attack\nheal\nrun\n";
+                //get the player input
+                getline(cin, input);
+                //if attack
+                if(input == "attack") {
+                    enemy.setHealth(enemy.getHealth() - (damage - enemy.getDefense()));
+                    cout << "You attacked for " << (damage - enemy.getDefense()) << " damage!\n";
+                }
+                else if(input == "heal") {
+                    health += rand() % 6 + 5;
+                    cout << "You healed!\n";
+                }
+                else if(input == "run") {
+                    cout << "You ran away!\n";
+                    break;
+                }
+                else {
+                    cout << "That is not an option.\n";
+                }
+                if(enemy.getHealth() > 0) {
+                    health -= (enemy.getDamage() - defense);
+                    cout << "That wild pokemon attacked you for " << (enemy.getDamage() - defense) << "damage!\n";
+                }
+
+            }  
+            if(health <= 0) {
+                cout << "Your pokemon fainted!\n";
+            }  
+            else if(enemy.getHealth() <= 0) {
+                cout << "You Won!!\n";
+            }
+            health = 20;
+            
+        }
+    }
 };
 
-
-
-//once i have the critter create a constructor
-
-//set it name 
-//set health, damage, hunger for starting values
-//mark the poke as alive
-
-//set name variable
-//get name variable
-
-//set health variable
-//get health variable
-
-//set damage variable 
-//get damage variable
-
-//set hunger variable
-//get hunger variable
-
-//set alive variable
-//get alive variable
-
 int main() {
-    poke myPoke("Charmander", 20, 5);
-    string input;
+    srand(time(0));
 
+    vector<string> pokeNames;
+    ifstream namesFile("names.txt");
+    string line;
+    while(getline(namesFile, line)) {
+        pokeNames.push_back(line);
+    }
+    namesFile.close();
+    //create a default variable for the poke variables
+    poke myPoke("Charmander", 15, 10, 3);
+    string input;
+    //Then create code that lets the player which pokemon they want
+    cout << "Hello I'm Professor Oak!\nLets chose your first Pokemon!\n";
+    cout << "Charmander!\n";
+    cout << "Bulbasaur!\n";
+    cout << "Squirtle!\n";
+    //get their input
+    getline(cin, input);
+    
+    //create an if and if/else statements where it states their health, offense, and defense
+    if(input == "Charmander") {
+        myPoke = poke("Charmander", 15, 10, 3);
+    }
+    else if(input == "Bulbasaur") {
+        myPoke = poke("Bulbasaur", 25, 5, 5);
+    }
+    else if(input == "Squirtle") {
+        myPoke = poke("Squirtle", 20, 7, 8);
+    }
+    else {
+        cout << "Sorry but we don't have that Pokemon.\n";
+    }
     while(true) {
         cout << "what do you want to do\n";
 
@@ -122,49 +237,21 @@ int main() {
             
         if(input == "feed") {
             myPoke.feed();
-            //if you feed, it lowers the number
-            //then mention that the critter ate
         }
         //create a 'training' function
         else if(input == "train") {
             cout << "You're gonna eat thunder,\nand crap lighting!\n";
-
-            //if hunger is above 10...
-            //say that the poke is starving
-            //if hunger is above 5 warn the player
-            //add 1 to the hunger level
-            //flip a coin
-                //heads = increase health
-                //talis = increase damage
-            //tell the player what has improved
+            myPoke.train();
         }
         //create a 'battle' function
         else if(input == "battle") {
             cout << "lets Get REady TO RUMMMMBBBLLEE!!\n";
+            myPoke.battle(pokeNames);
 
-            //if hunger is above 10 stop and say its hungry
-            //if hunger is above 5, warn the player but continue add 1 to the hunger
-            //keep going back and fourth in the battle
-            //until someone wins or the player runs
-                //ask the player
-                    //attack
-                    //heal
-                    //or run 
-                //if attack - subtract damage from enemy health
-                //if heal - add some health back 
-                //if run - end battle
-                //if the enemy is alive, it fights back
-                //check to see if either side has fallen in battle
-                //end the battle
         }
         //create a 'listen' function
         else if(input == "listen"){
-            //show name, health, damage, and hunger
-            //if hunger goes above 5
-            //have the program mention it
-            cout << "I'm feeling good\n";
-            cout << "I'm a bit hungry\n";
-            cout << "FEED ME NOW!!\n";
+           myPoke.listen();
         }
         else if(input == "quit") {
             cout << "Thanks for playing\n";
@@ -179,47 +266,3 @@ int main() {
 
     return 0;
 }
-
-
-
-
-
-
-//Write out the different functions for the poke
-
-
-
-
-
-
-
-
-
-
-
-
-
-//main 
-
-//start
-    //open the names file
-    //read the name in the list
-    //then close the file
-
-
-//name critter function
-    //ask the player for input if they wish to name the poke
-    //if random every, pick one in the list
-    //create poke with that name
-
-
-//create a while loop (repeat this until player is done)
-    //show the options to the player
-        //feed
-        //train
-        //listen
-        //battle
-        //quit
-    //do what the player asked
-    //if battle choose a poke at random from the names on the list
-
